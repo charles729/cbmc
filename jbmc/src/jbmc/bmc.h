d3883dc4497839f1248193c8d9f4f9a2c2a61bb7
Peter Schrammel <peter.schrammel@diffblue.com>
2018-12-09 16:35:00 +0000

    Call preprocess field_decl
    We are modifying the model inside goto-checker;
    thus, we pass a non-abstract goto_model.

--- jbmc/src/jbmc/bmc.h
+++ jbmc/src/jbmc/bmc.h
@@ -129,6 +129,11 @@ public:
       nullptr,
     std::function<bool(void)> callback_after_symex = nullptr);
 
+  void set_symex_fields(const std::map<irep_idt, typet> &fields)
+  {
+    symex.fields = fields;
+  }
+
 protected:
   /// \brief Constructor for path exploration from saved state
   ///
--- src/goto-checker/all_properties_verifier.h
+++ src/goto-checker/all_properties_verifier.h
@@ -25,7 +25,7 @@ public:
   all_properties_verifiert(
     const optionst &options,
     ui_message_handlert &ui_message_handler,
-    abstract_goto_modelt &goto_model)
+    goto_modelt &goto_model)
     : goto_verifiert(options, ui_message_handler),
       goto_model(goto_model),
       incremental_goto_checker(options, ui_message_handler, goto_model)
@@ -55,7 +55,7 @@ public:
   }
 
 protected:
-  abstract_goto_modelt &goto_model;
+  goto_modelt &goto_model;
   incremental_goto_checkerT incremental_goto_checker;
   std::size_t iterations = 1;
 };
